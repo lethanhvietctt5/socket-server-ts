05e5129253530a68440485b6bd6748e1a88a45c0
Le Thanh Viet <lethanhviet7c@gmail.com>
2022-01-03 17:10:50 +0700

    Fix jwt access_token

--- src/middleware/auth.mdw.ts
+++ src/middleware/auth.mdw.ts
@@ -7,25 +7,33 @@ export default async function authMiddleware(req: Request, res: Response, next: 
   const access_token: string | undefined = req.headers["x-auth-token"]?.toString();
 
   if (typeof access_token === "undefined") {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: "Missing access_token" });
   }
 
-  const decode = jwt.verify(access_token, "secret");
-  const { email, expriredAt } = decode as Token;
-
-  if (expriredAt < Date.now()) {
-    return res.status(401).json({
-      message: "Token expired",
-    });
+  if (access_token.length === 0) {
+    return res.status(401).json({ message: "access_token is empty." });
   }
 
-  const user = await DAO.userDAO.getUserByEmail(email);
+  try {
+    const decode = jwt.verify(access_token, "secret");
+    const { email, expriredAt } = decode as Token;
 
-  if (user === null) {
-    return res.status(401).json({ message: "Unauthorized" });
+    if (expriredAt < Date.now()) {
+      return res.status(401).json({
+        message: "Token expired",
+      });
+    }
+
+    const user = await DAO.userDAO.getUserByEmail(email);
+
+    if (user === null) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    res.locals.user_email = email;
+
+    return next();
+  } catch (e) {
+    return res.status(401).json({ message: "Invalid access_token" });
   }
-
-  res.locals.user_email = email;
-
-  return next();
 }
--- src/routes/authentication/auth.ts
+++ src/routes/authentication/auth.ts
@@ -8,31 +8,39 @@ const authRoute = Router();
 authRoute.post("/", async (req: Request, res: Response) => {
   const access_token: string | undefined = req.headers["x-auth-token"]?.toString();
 
-  if (typeof access_token === "undefined" || access_token.length === 0) {
-    return res.status(401).json({ message: "Unauthorized" });
+  if (typeof access_token === "undefined") {
+    return res.status(401).json({ message: "Missing access_token" });
   }
 
-  const decode = jwt.verify(access_token, "secret");
-  const { email, expriredAt } = decode as Token;
+  if (access_token.length === 0) {
+    return res.status(401).json({ message: "access_token is empty." });
+  }
 
-  if (expriredAt < Date.now()) {
-    return res.status(401).json({
-      message: "Token expired",
+  try {
+    const decode = await jwt.verify(access_token, "secret");
+    const { email, expriredAt } = decode as Token;
+
+    if (expriredAt < Date.now()) {
+      return res.status(401).json({
+        message: "Token expired",
+      });
+    }
+
+    const user = await DAO.userDAO.getUserByEmail(email);
+
+    if (user === null) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const userJSON = await DAO.userDAO.toJSON(user);
+
+    return res.status(200).json({
+      message: "Authenticated",
+      user: userJSON,
     });
+  } catch (e) {
+    return res.status(401).json({ message: "Invalid access_token" });
   }
-
-  const user = await DAO.userDAO.getUserByEmail(email);
-
-  if (user === null) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  const userJSON = await DAO.userDAO.toJSON(user);
-
-  return res.status(200).json({
-    message: "Authenticated",
-    user: userJSON,
-  });
 });
 
 export default authRoute;
